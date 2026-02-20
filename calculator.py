fe98c119e0fdbc21162512a6f0720395a656cba8
JosephRidge <josephridge18@gmail.com>
2026-02-20 09:25:24 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -8,8 +8,9 @@
 """
 
 print("============================================")
-valueOne = input("Enter the first operand")
-valueTwo = input("Enter the second operand")
+valueOne = input("Enter the first operand: ")
+valueTwo = input("Enter the second operand: ")
+
 operatorChoice = input(
 """
 Kindly input the number of the operation you want: \n
@@ -21,6 +22,26 @@ Kindly input the number of the operation you want: \n
  6. floor division(divsion without decimals //)
  
 """)
+output = "" # global variable
 
+if (operatorChoice == 1):
+    output = valueOne + valueTwo
+elif (operatorChoice == 2):
+    output = valueOne - valueTwo
+elif (operatorChoice == 3):
+    output = valueOne * valueTwo
+elif (operatorChoice == 4):
+    output = valueOne / valueTwo
+elif (operatorChoice == 5):
+    output = valueOne ** valueTwo
+elif (operatorChoice == 6):
+    output = valueOne // valueTwo
+else: 
+    output ="wrong selection!"
+    return 
+
+print("*****************************")
+print(output)
+print("*****************************")
 
 print("============================================")
